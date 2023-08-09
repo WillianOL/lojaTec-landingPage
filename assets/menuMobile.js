f9802323bf9d3e16bf3158f29dc92d2724866857
Will Oliveira <williancontato67@gmail.com>
2023-08-09 20:15:30 -0300

    Update menuMobile.js

--- assets/menuMobile.js
+++ assets/menuMobile.js
@@ -4,6 +4,7 @@ const botaoMobile = document.querySelector(".btnMobile span");
 function abrirEFecharMenu() {
     menuMobile.classList.toggle("ativo")
     botaoMobile.classList.toggle("ativo")
+
     if(botaoMobile.classList.contains("ativo")){
         botaoMobile.innerHTML = "close"
     } else{
